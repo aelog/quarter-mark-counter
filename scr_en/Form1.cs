129e5780757d6aac804ba7d83afe4e7c7b7b7e56
aelog <168465552+aelog@users.noreply.github.com>
2025-04-12 03:06:44 +0300

    Add files via upload

---
+++ scr_en/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quarter_Mark_Counter_GUI.NET_
+{
+    public partial class Form1 : Form
+    {
+
+        public float inp_mark = 0;
+        public float inp_coef = 0;
+        public string q_m = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                inp_coef = Convert.ToSingle(((TextBox)sender).Text);
+            }
+            catch (System.FormatException)
+            {
+                inp_coef = 0;
+            }
+        }
+
+        private void count_button_Click(object sender, EventArgs e)
+        {
+            q_m = Program.count_mark(inp_mark, inp_coef);
+            MessageBox.Show($"Your quarter mark is {q_m}", "Quarter mark");
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            inp_mark = Convert.ToSingle(numericUpDown1.Value);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("1.5 - Admin. CW, Practic CW, CW, Control Dictation, Class essay, Test.\r\n1.4 - Audition, Home essay, Vocabulary Dictation, Exposition.\r\n1.3 - Lab. work, Practic Work, Verification Work.\r\n1.2 - Independent Work.\r\n1.0 - Work on the lesson, HW, Test, Labeling, Dist. lesson, E+Learning.", "Coefficients");
+        }
+
+        private void Reset_button_Click(object sender, EventArgs e)
+        {
+            Program.reset();
+        }
+
+        private void watch_button_Click(object sender, EventArgs e)
+        {
+            q_m = Program.count_mark(0, 0);
+            MessageBox.Show($"Your quarter mark is {q_m}", "Quarter mark");
+        }
+
+        private void about_button_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Quarter Mark Counter v. 1.0\n" +
+                "This program is licensed under the MIT license.\n" +
+                "(C) AntELO, 2025. All right reserved", "О программе");
+        }
+    }
+}
---
+++ scr_en/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quarter_Mark_Counter_GUI.NET_
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+
+        static void Main()
+        {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        public static float sum = 0;
+        public static float div = 0;
+
+        public static string count_mark(float mark, float coef) 
+        {
+            Console.WriteLine(Convert.ToString(mark));
+            Console.WriteLine(Convert.ToString(coef));
+            string out_mark = "";
+
+            mark *= coef;
+            Console.WriteLine(Convert.ToString(mark));
+            sum += mark;
+            div += coef;
+            Console.WriteLine(Convert.ToString(sum));
+            Console.WriteLine(Convert.ToString(div));
+            out_mark = Convert.ToString(Convert.ToSingle(Math.Round(sum / div, 2)));
+            Console.WriteLine(out_mark);
+            return out_mark;
+        }
+
+        public static void reset()
+        {
+            sum = 0;
+            div = 0;
+        }
+    }
+}
